c4b9fe70f5fb18e60da364be0791b9c6837a4971
Carlos Henrique Pinheiro Cordeiro <carloshenpincord@gmail.com>
2023-07-02 14:38:16 -0300

    Criação as instâncias do MongoDB e primeiros testes

--- persistencia-ws/PersistenciaContatoService.py
+++ persistencia-ws/PersistenciaContatoService.py
@@ -1,5 +1,6 @@
 import cherrypy
 import json
+from pymongo import MongoClient
 
 class PersistenciaContatoService(object):
 
@@ -17,12 +18,28 @@ class PersistenciaContatoService(object):
         "Processa a inclusão de um novo contato"
         return "Hello World"
 
+    def incluiNovoTxt(self):
+        #inclui o novo contato no arquivo txt
+        print("Hello World")
+
+    def incluiNovoBanco(self):
+        #inclui o novo contato no banco
+        print("Hello World")
+
     @cherrypy.expose
     @cherrypy.tools.json_out()
     @cherrypy.tools.json_in()
     def processaInclusaoExistente(self, iId, sDescricao):
         "Adiciona um contato a um já existente, através do ID"
         return "Hello World"
+
+    def incluiExistenteTxt(self):
+        #inclui o contato para um já existente no txt
+        print("Hello World")
+
+    def incluiExistenteBanco(self):
+        #inclui o contato para um já existente no banco
+        print("Hello World")
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
@@ -37,8 +54,12 @@ class PersistenciaContatoService(object):
     def processaExclusaoTotalContato(self, sNome, iIdContato):
         "Processa a exclusão total de um contato, excluindo-o totalmente através do ID"
         return "Hello World"
-    
-if __name__ == '__main__':
-    config = {'server.socket_host': '0.0.0.0'}
-    cherrypy.config.update(config)
-    cherrypy.quickstart(PersistenciaContatoService())
+
+#if __name__ == '__main__':
+#    config = {'server.socket_host': '0.0.0.0'}
+#    cherrypy.config.update(config)
+#    cherrypy.quickstart(PersistenciaContatoService())
+
+client = MongoClient("localhost", 8085)
+for data in client.contatos.get_collection("contatos").find():
+    print(data)
